81ea61108b6fd0835adaad290222cb4dd876c540
ohyasswecan <63643014+ohyasswecan@users.noreply.github.com>
2023-10-19 11:59:34 +1300

    New Changes done, new invoice is not done yet

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
+const cors = require('cors'); // Import the cors package
 
 app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
 
 app.post('/save', (req, res) => {
   const xmlData = req.body.xmlData; // XML data sent from the client
-  fs.writeFileSync('Boat.xml', xmlData);
+  fs.writeFile('Boat.xml', xmlData, (err) => {
+    if (err) {
+      console.error('Error writing to Boat.xml:', err);
+      res.status(500).send('Error writing to Boat.xml');
+    } else {
+      console.log('XML data saved successfully.');
+      res.send('XML data saved successfully.');
+    }
+  });
+
+
+  // Send the PUT request
+  xmlhttp.send('new xml string');
   res.send('XML data saved successfully.');
 });
 
@@ -14,20 +27,3 @@ app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
 
-function saveXMLData(xmlData) {
-    fetch('http://localhost:3000/save', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ xmlData }),
-    })
-      .then(response => response.text())
-      .then(data => {
-        console.log(data); // Server response message
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-  
