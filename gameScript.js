b92787d819b784f24309ccee5591fc881558fa12
LiemB1 <brundon22.22@gmail.com>
2023-10-27 23:36:36 +1300

    Added extremely simple enemy movement and spawning

--- gameScript.js
+++ gameScript.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
 
+const numberOfEnemies = 20;
+const enemyArray = [];
+
 const player = {
 	width: 30,
 	height: 30,
@@ -12,6 +15,33 @@ const player = {
 	dyu: 0,
 	dyd: 0
 };
+
+class Enemy {
+	constructor() {
+		this.x = Math.random() * canvas.width;
+		this.y = Math.random() * canvas.height;
+		this.width = 10;
+		this.height = 10;
+		this.speed = Math.random() * 2 + 1;
+		this.angle = 0;
+		this.angleSpeed = Math.random() * 0.2;
+		this.curve = Math.random() * 6;
+	}
+	update() {
+		this.x += this.curve * Math.sin(this.angle);
+		this.y -= this.speed;
+		this.angle += this.angleSpeed;
+		if(this.y + this.height < 0) this.y = canvas.height;
+	}
+	
+	draw() {
+		ctx.strokeRect(this.x, this.y, this.width, this.height);
+	}
+}
+
+for (let i = 0; i < numberOfEnemies; i++) {
+	enemyArray.push(new Enemy());
+}
 
 function drawPlayer() {
 	ctx.fillRect(player.x, player.y, player.width, player.height);
@@ -58,6 +88,11 @@ function update() {
 	drawPlayer();
 	
 	newPos();
+	
+	enemyArray.forEach(enemy => {
+		enemy.update();
+		enemy.draw();
+	});
 	
 	requestAnimationFrame(update);
 }
