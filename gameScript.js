0d47e282a1db3a87d22bd6e90ea661776218ed2f
LiemB1 <brundon22.22@gmail.com>
2023-10-27 22:16:15 +1300

    Categorised code
    Added classes, functions calling other functions to further tidy up code

--- gameScript.js
+++ gameScript.js
@@ -1,43 +1,109 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
 
-let x = 600;
-let y = 450;
-let vxl = 0;
-let vxr = 0;
-let vyu = 0;
-let vyd = 0;
+const player = {
+	width: 30,
+	height: 30,
+	x: 600,
+	y: 450,
+	speed: 5,
+	dxl: 0,
+	dxr: 0,
+	dyu: 0,
+	dyd: 0
+};
+
+function drawPlayer() {
+	ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+
+function clear() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function newPos() {
+	player.x += player.dxl;
+	player.x += player.dxr;
+	player.y += player.dyu;
+	player.y += player.dyd;
+	
+	detectEdge();
+}
+
+function detectEdge() {
+	//Left canvas edge
+	if (player.x < 0) {
+		player.x = 0;
+	}
+	
+	//Right canvas edge
+	if (player.x + player.width > canvas.width) {
+		player.x = canvas.width - player.width;
+	}
+	
+	//Top canvas edge
+	if (player.y < 0) {
+		player.y = 0;
+	}
+	
+	//Bottom canvas edge
+	if (player.y + player.height > canvas.height) {
+		player.y = canvas.height - player.height;
+	}
+}
 
 function update() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	x += vxl;
-	x += vxr;
-	y += vyu;
-	y += vyd;
-	ctx.fillRect(x, y, 30, 30);
+	clear();
+
+	drawPlayer();
+	
+	newPos();
+	
 	requestAnimationFrame(update);
 }
+
+function moveRight() {
+	player.dxr = player.speed;
+}
+
+function moveLeft() {
+	player.dxl = -player.speed;
+}
+
+function moveUp() {
+	player.dyu = -player.speed;
+}
+
+function moveDown() {
+	player.dyd = player.speed;
+}
+
+function keyDown(e) {
+	if (e.key == "ArrowRight") {
+		moveRight();
+	} else if (e.key == "ArrowLeft") {
+		moveLeft();
+	} else if (e.key == "ArrowUp") {
+		moveUp();
+	} else if (e.key == "ArrowDown") {
+		moveDown();
+	}
+	
+}
+
+function keyUp(e) {
+	if (e.key =="ArrowRight") {
+		player.dxr = 0;
+	} else if (e.key == "ArrowLeft") {
+		player.dxl = 0;
+	} else if (e.key == "ArrowUp") {
+		player.dyu = 0;
+	} else if (e.key == "ArrowDown") {
+		player.dyd = 0;
+	}
+}
+
 update();
 
-addEventListener("keydown", function(e){
-	if (e.code == "KeyD" || e.code == "ArrowRight")
-		vxr = 5;
-	if (e.code == "KeyA" || e.code == "ArrowLeft")
-		vxl = -5;
-	if (e.code == "KeyW" || e.code == "ArrowUp")
-		vyu = -5;
-	if (e.code == "KeyS" || e.code == "ArrowDown")
-		vyd = 5;
-})
-
-addEventListener("keyup", function(e){
-	if (e.code == "KeyD" || e.code == "ArrowRight")
-		vxr = 0;
-	if (e.code == "KeyA" || e.code == "ArrowLeft")
-		vxl = 0;
-	if (e.code == "KeyW" || e.code == "ArrowUp")
-		vyu = 0;
-	if (e.code == "KeyS" || e.code == "ArrowDown")
-		vyd = 0;
-	
-})
+addEventListener("keydown", keyDown);
+addEventListener("keyup", keyUp);
